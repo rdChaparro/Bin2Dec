dba9a64ad1df17700b27bd5198356c6050a0b8f3
rdChaparro <rdchaparro7@misena.edu.co>
2024-04-25 18:44:47 -0500

    Class binary to decimal complete

--- src/model/convertToInteger.java
+++ src/model/convertToInteger.java
@@ -1,7 +1,18 @@
 package model;
 
 public class ConvertToInteger {
-	public String toInt (String x) {
-		return x;
+	public String toInt(String x) {
+
+		int res = 0;
+		int count = 1;
+
+		for (int n = x.length() - 1; n >= 0; n--) {
+
+			int numberDec = Character.getNumericValue(x.charAt(n));
+			res += count * numberDec;
+			count *= 2;
+		}
+
+		return Integer.toString(res);
 	}
 }
--- src/view/MenuInsertDec.java
+++ src/view/MenuInsertDec.java
@@ -10,26 +10,24 @@ public class MenuInsertDec {
 
 		ConvertToBinary tobinary = new ConvertToBinary();
 		MenuBinary binaryMenu = new MenuBinary();
-		
 		Scanner scannerCall = new Scanner(System.in);
-		
-		try {			
+
+		try {
 			System.out.println("");
 			System.out.println("*".repeat(50));
 			System.out.println("Ingresa un numero decimal valido:");
-			
+
 			String eleccion = scannerCall.next();
-			
+
 			System.out.println("*".repeat(50));
-			System.out.println("El numero en binario es: \n" );
+			System.out.println("El numero en binario es: \n");
 			System.out.println(tobinary.toBinary(eleccion));
-			
+
 			binaryMenu.menu();
-			
+
 		} finally {
 			scannerCall.close();
 		}
 	}
 
-	
 }
